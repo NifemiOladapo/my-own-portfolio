30d829d3795fa212d19814e498a327168a24becd
Nifnix <119703179+NifemiOladapo@users.noreply.github.com>
2025-01-12 14:30:36 -0800

    Update layout.tsx
    Added metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,16 @@
 import './globals.css'
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Nifemi Oladapo | Professional Full stack Web Developer & Frontend Developer",
+  description:
+    "Welcome to Nifemi Oladapo's Portfolio! Explore my work as a talented senior full-stack MERN and Next.js web developer, creating engaging, performance-driven web applications. Let's bring your ideas to life!",
+  keywords:
+    "Full-stack developer, MERN stack, Next.js developer, React.js developer, web development, portfolio, software engineer, frontend developer, backend developer, JavaScript developer, modern web applications, responsive design, UI/UX design, freelance developer, coding projects, personal portfolio, web developer, Nifemi Oladapo, tech enthusiast, programming, web solutions, scalable applications,
+ other: {
+    "google-site-verification": "idIMo8Af4EGCmLyDfn9uxSdadc27e3cCnb7AWQhCp3k",
+  }
+};
 
 
 export default function RootLayout({
