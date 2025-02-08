4cd4237f9f06a7dec0d1ce8cfdac153bdd3ea4e7
nifemi oladapo <nifethedev@gmail.com>
2025-02-08 11:23:05 -0800

    updated app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,6 @@
 import { Metadata } from "next";
 import "../app/globals.css";
-import './globals.css'
-
-export const metadata: Metadata = {
-  title: "Nifemi Oladapo | Professional Full stack Web Developer & Frontend Developer",
-  description:
-    "Welcome to Nifemi Oladapo's Portfolio! Explore my work as a talented senior full-stack MERN and Next.js web developer, creating engaging, performance-driven web applications. Let's bring your ideas to life!",
-  keywords:
-    "Full-stack developer, MERN stack, Next.js developer, React.js developer, web development, portfolio, software engineer, frontend developer, backend developer, JavaScript developer, modern web applications, responsive design, UI/UX design, freelance developer, coding projects, personal portfolio, web developer, Nifemi Oladapo, tech enthusiast, programming, web solutions, scalable applications"
-};
+import "./globals.css";
 
 export const metadata: Metadata = {
   title:
@@ -16,11 +8,11 @@ export const metadata: Metadata = {
   description:
     "Discover the stunning capabilities and projects of Oladapo Nifemi, a professional and well-fitted full stack web developer and software developer with expertise in building scalable and efficient software solutions.",
   keywords:
-    "Backend developer, frontend developer, software development, web development, Oladapo Nifemi, nextjs developer, nodejs developer, React developer, MERN stack development, full stack development",
+    "Full-stack developer, MERN stack, MERN developer,  web developer, Oladapo Nifemi, React Developer, Backend developer, frontend developer, software development, web development,  nextjs developer, nodejs developer, React developer, MERN stack development, full stack development",
 };
 
 export default function RootLayout({
-  children, 
+  children,
 }: {
   children: React.ReactNode;
 }) {
